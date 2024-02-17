73f63e28b2ba1f252d58161f70d3244f4c6176db
Vinicius.Klein <Vinicius.Klein@AEPNBW10036.aeplus.local>
2024-02-17 12:21:47 -0300

    execism 04 done

--- exercism4/script.js
+++ exercism4/script.js
@@ -1,14 +1,37 @@
-const hoursWorked = 8
-function dayRate(day) {
-    return day * hoursWorked
+const horaTrabalhada = 8
+
+function taxaDiaria(taxaHoraria) {
+    return taxaHoraria * horaTrabalhada
 }
 
-function daysInBudget(totalBudget, day) {
-    return Math.floor(totalBudget / (dayRate(day)))
+function diasDeProjeto(orcamentoFixo, taxaHoraria) {
+    return Math.floor(orcamentoFixo / (taxaDiaria(taxaHoraria)))
 }
 
-let newDay = 89
-let newTotalBudget = 20000
 
-console.log(daysInBudget(newTotalBudget, newDay))
-console.log('teste')
+function precoTotal(taxaHoraria, numeroDeDias, porcentagem) {
+    let valorComDesconto = taxaDiaria(taxaHoraria) * numeroDeDias * (1 - (porcentagem / 100))
+    let valorRestante = taxaDiaria(taxaHoraria) * numeroDeDias % 22
+    return Math.ceil(valorRestante + valorComDesconto)
+}
+
+
+// console.log('taxa diaria: ', taxaDiaria(taxaHoraria))
+// console.log('dias de Projeto: ', diasDeProjeto(orcamentoFixo, taxaHoraria))
+console.log('Preço total: ', precoTotal(89, 230, 42))
+
+
+
+
+
+// function discoutedRateLargeProject(day, ){
+//     var diasRestantes = day%22
+//     var mesesTotais = Math.floor(day/22)
+
+//     var desconto = mesesTotais * discount/100
+// }
+
+// console.log(daysInBudget(newTotalBudget, newDay))
+
+// //every month has 22 billable days
+
